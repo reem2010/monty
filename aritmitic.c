52950203c1f891e192e0feeb384bfca1f8a31fc1
reem <tarekreem950@gmail.com>
2023-06-22 16:14:50 -0700

    arithmetic operations

--- aritmitic.c
+++ aritmitic.c
@@ -30,7 +30,7 @@ void sub(stack_t **stack, unsigned int l)
 	if (!(*stack) || !((*stack)->prev))
 	{
 		free_list(*stack), fflush(stdout);
-		fprintf(stderr, "L%d: can't add, stack too short\n", l);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", l);
 		fclose(fp), exit(EXIT_FAILURE);
 	}
 	sub = (((*stack)->prev)->n) - ((*stack)->n);
@@ -49,7 +49,7 @@ void mul(stack_t **stack, unsigned int l)
 	if (!(*stack) || !((*stack)->prev))
 	{
 		free_list(*stack), fflush(stdout);
-		fprintf(stderr, "L%d: can't add, stack too short\n", l);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", l);
 		fclose(fp), exit(EXIT_FAILURE);
 	}
 	mul = ((*stack)->n) * (((*stack)->prev)->n);
@@ -68,7 +68,7 @@ void divide(stack_t **stack, unsigned int l)
 	if (!(*stack) || !((*stack)->prev))
 	{
 		free_list(*stack), fflush(stdout);
-		fprintf(stderr, "L%d: can't add, stack too short\n", l);
+		fprintf(stderr, "L%d: can't div, stack too short\n", l);
 		fclose(fp), exit(EXIT_FAILURE);
 	}
 
